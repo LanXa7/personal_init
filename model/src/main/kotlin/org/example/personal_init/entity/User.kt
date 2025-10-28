16f8b7315444da292f4213cdb103c80d95404605
lanxa7 <lanxinhui@musetech.group>
2025-10-28 14:42:08 +0800

    ✨ feat(db): 为用户实体添加数据库表映射
    - 添加 `@Table(name = "\"user\"")` 注解
    - 明确指定数据库表名为 "user"
    - 解决可能的表名冲突问题

--- model/src/main/kotlin/org/example/personal_init/entity/User.kt
+++ model/src/main/kotlin/org/example/personal_init/entity/User.kt
@@ -2,9 +2,11 @@ package org.example.personal_init.entity
 
 import org.babyfish.jimmer.sql.Entity
 import org.babyfish.jimmer.sql.OneToMany
+import org.babyfish.jimmer.sql.Table
 import org.example.personal_init.base.BaseEntity
 
 @Entity
+@Table(name = "\"user\"")
 interface User : BaseEntity {
 
     val username: String
