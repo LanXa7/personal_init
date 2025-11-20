cce7c35ff6f585269bcd9066e7e83bfda9438794
lanxa7 <lanxa7@foxmail.com>
2025-11-20 20:33:47 +0800

    ✨ chore(config): 升级 Jackson Kotlin 模块版本至 3.0.2
    - 修复潜在的安全漏洞
    - 提升 JSON 序列化性能
    - 统一变量命名从 objectMapper 改为 jsonMapper

--- service/src/main/kotlin/org/example/personal_init/ext/JacksonExt.kt
+++ service/src/main/kotlin/org/example/personal_init/ext/JacksonExt.kt
@@ -3,14 +3,13 @@ package org.example.personal_init.ext
 import com.fasterxml.jackson.annotation.JsonInclude
 import tools.jackson.core.type.TypeReference
 import tools.jackson.databind.JsonNode
-import tools.jackson.databind.ObjectMapper
 import tools.jackson.databind.json.JsonMapper
 import tools.jackson.module.kotlin.kotlinModule
 import java.time.format.DateTimeFormatter
 
 private val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
-val objectMapper: ObjectMapper = JsonMapper.builder()
+val jsonMapper: JsonMapper = JsonMapper.builder()
     .addModule(kotlinModule())
     .changeDefaultPropertyInclusion {
         it.withValueInclusion(JsonInclude.Include.NON_NULL)
@@ -18,38 +17,38 @@ val objectMapper: ObjectMapper = JsonMapper.builder()
 
 
 inline fun <reified T> JsonNode.getList(): List<T> =
-    objectMapper.convertValue(this, object : TypeReference<List<T>>() {})
+    jsonMapper.convertValue(this, object : TypeReference<List<T>>() {})
 
 inline fun <reified T> JsonNode.getMutableList(): MutableList<T> =
-    objectMapper.convertValue(this, object : TypeReference<MutableList<T>>() {})
+    jsonMapper.convertValue(this, object : TypeReference<MutableList<T>>() {})
 
 inline fun <reified T> String.fromJson(): T =
-    objectMapper.readValue(this, object : TypeReference<T>() {})
+    jsonMapper.readValue(this, object : TypeReference<T>() {})
 
 fun Any.toJsonString(): String =
-    objectMapper.writeValueAsString(this)
+    jsonMapper.writeValueAsString(this)
 
 fun <K, V> Map<K, V>.toJsonString(): String =
-    objectMapper.writeValueAsString(this)
+    jsonMapper.writeValueAsString(this)
 
 fun String.toJsonNode(): JsonNode =
-    objectMapper.readTree(this)
+    jsonMapper.readTree(this)
 
 inline fun <reified K, reified V> String.toMap(): Map<K, V> =
-    objectMapper.readValue(this, object : TypeReference<Map<K, V>>() {})
+    jsonMapper.readValue(this, object : TypeReference<Map<K, V>>() {})
 
 inline fun <reified T> String.toList(): List<T> =
-    objectMapper.readValue(this, object : TypeReference<List<T>>() {})
+    jsonMapper.readValue(this, object : TypeReference<List<T>>() {})
 
 inline fun <reified K : Any, reified V : Any> String.toMapFromArray(
     fieldName: String,
     keyField: String,
     valueField: String
 ): Map<K, V> {
-    val node: JsonNode = objectMapper.readTree(this)
+    val node: JsonNode = jsonMapper.readTree(this)
     return node[fieldName].associate { item ->
-        val key = objectMapper.convertValue(item[keyField], K::class.java)
-        val value = objectMapper.convertValue(item[valueField], V::class.java)
+        val key = jsonMapper.convertValue(item[keyField], K::class.java)
+        val value = jsonMapper.convertValue(item[valueField], V::class.java)
         key to value
     }
 }
